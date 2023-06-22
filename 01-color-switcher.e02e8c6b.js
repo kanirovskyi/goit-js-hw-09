137546246ac7abe1f529ea2610c8459ced4c4d61
kanirovskyi <124185246+kanirovskyi@users.noreply.github.com>
2023-06-22 14:04:13 +0000

    Deploying to gh-pages from @ kanirovskyi/goit-js-hw-09@5324fc1dc3a06baf7e057d98f0fd0d0a1ac9f98d 🚀

---
+++ 01-color-switcher.e02e8c6b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");a.disabled=!0;var d=null;e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,d=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),a.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,a.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.e02e8c6b.js.map
